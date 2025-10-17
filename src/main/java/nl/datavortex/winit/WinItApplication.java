4bae4d942ccf2ee7225ed5ba69d823a39684c7ec
RowanPl <rowanplooij@hotmail.com>
2025-10-17 15:05:56 +0200

    init

---
+++ src/main/java/nl/datavortex/winit/WinItApplication.java
@@ -0,0 +1,13 @@
+package nl.datavortex.winit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/datavortex/winit/WinItApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.datavortex.winit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
